e36ac731ef26046fc09ee1bfd73e1377e2ae126f
Dragos Ciobanu <dragos.ciobanu@sap.com>
2021-06-12 20:09:04 +0200

    Add password is common service scaffolding

---
+++ password_common_svc/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Password is common service');
+});
+
+app.post("/api/password/score", async (req, res, next) => {
+    try {
+        const passwordText: string = req.body.password;
+        res.json({passwordText});
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.listen(port, () => {
+    return console.log(`server is listening on ${port}`);
+});
